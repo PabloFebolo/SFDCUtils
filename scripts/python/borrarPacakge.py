9af2c4e0af600ea0824fe60e9c3a1eb709aefc98
Pablo Fébolo <pfebolo@yahoo.com>
2024-04-16 16:18:12 -0300

    configuración externa de los typos de componentes

--- scripts/python/borrarPacakge.py
+++ scripts/python/borrarPacakge.py
@@ -1,6 +1,7 @@
 import xml.etree.ElementTree as ET
 import argparse
 import os
+import json
 
 def eliminar_nodos_por_criterio(root, namespace, nodo_padre, nodo_hijo, texto_inicio):
     removed_count = 0
@@ -18,21 +19,6 @@ def eliminar_varios_tipos_nodos(root, namespace, textos_inicio, nodos_config):
             removed_count += eliminar_nodos_por_criterio(root, namespace, nodo_padre, nodo_hijo, texto_inicio)
     return removed_count
 
-nodos_config = [
-    ('applicationVisibilities', 'application'),
-    ('classAccesses', 'apexClass'),
-    ('fieldPermissions', 'field'),
-    ('layoutAssignments', 'layout'),
-    ('tabVisibilities', 'tab'),
-    ('pageAccesses', 'apexPage'),
-    ('customMetadataTypeAccesses', 'name'),
-    ('customPermissions', 'name'),
-    ('customSettingAccesses', 'name'),
-    ('flowAccesses', 'flow'),
-    ('objectPermissions', 'object'),
-    ('recordTypeVisibilities', 'recordType')
-]
-
 parser = argparse.ArgumentParser(description="Elimina nodos específicos de un archivo XML")
 parser.add_argument('--pkg', type=lambda s: s.split(','), required=True, help='Texto inicial para identificar nodos a eliminar, separado por comas')
 parser.add_argument('--profile', type=str, required=True, help='Ruta al archivo de perfil XML a procesar')
@@ -41,13 +27,16 @@ args = parser.parse_args()
 NAMESPACE = "http://soap.sforce.com/2006/04/metadata"
 xml_path = args.profile
 
-# Generar el nombre de archivo de salida
+# Cargar configuración de nodos desde JSON
+json_path = 'componentTypesPermissions.json'
+with open(json_path, 'r') as file:
+    data = json.load(file)
+    nodos_config = [(item['node'], item['subnode']) for item in data['types']]
+
 base_dir = os.path.dirname(xml_path)
 base_name = os.path.basename(xml_path)
 file_root, file_extension = os.path.splitext(base_name)
-
-# Insertar 'New' antes del primer punto en el nombre del archivo
-first_part = file_root.split('.')[0] + "New"  # Añadir 'New' a la primera parte
+first_part = file_root.split('.')[0] + "New"
 remaining_parts = '.'.join(file_root.split('.')[1:]) if '.' in file_root else ''
 output_filename = f"{first_part}.{remaining_parts}{file_extension}"
 output_path = os.path.join(base_dir, output_filename)
